d77d01d5c24a891aa12dde0c61a187ddf36ac9b9
Determinant <tederminant@gmail.com>
2020-05-22 11:05:08 -0400

    support python2

--- name2color.py
+++ name2color.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import struct
 import hashlib
 # add your favorite color code (256) here as candidates
 colors = [
@@ -8,4 +9,8 @@ colors = [
     160, 161, 167, 137, 131
 ]
 h = hashlib.sha1(sys.argv[1].encode('utf-8')).digest()
-print("colour{}".format(colors[h[0] % len(colors)]))
+if sys.version_info.major == 3:
+    hn = int.from_bytes(h, byteorder='big')
+else:
+    (hn,) = struct.unpack('>Q12x', h)
+print("colour{}".format(colors[hn % len(colors)]))
